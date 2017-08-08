8d922f5c9b2bfe5e7e0f53d0ae591929d3fd21bb
vagrant <vagrant@vagrant.vm>
2017-08-08 17:27:02 +0000

    Second Commit

--- application.py
+++ application.py
@@ -186,7 +186,7 @@ def gconnect():
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
-    login_session['provider']=data['google']
+    #login_session['provider']=data['google']
 
     output = ''
     output += '<h1>Welcome, '
